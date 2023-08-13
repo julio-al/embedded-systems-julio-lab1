c48167a8f4ab9bbb7992416379e77e66f3c82cfb
julio-al <julioc-alves@hotmail.com>
2023-08-13 16:17:14 +0000

    lab 1 part 2

--- main.cpp
+++ main.cpp
@@ -22,17 +22,34 @@
 //or EventQueue to defer the function call to different context 
 //or switch to bare metal profile that does not use RTOS or mutexes
 
+//Lab 1 part 2
+//The reason why the program is not working as expected is because the flag variable is shared between 
+//the main funtion and the interrupt handler. This means that there is a possibility of a race condition, 
+//where the flag value is changed by one thread while the other thread is reading or writing it. 
+//This can lead to inconsistent or incorrect results. To avoid this problem, the program need to ensure that the access 
+//to the flag variable is atomic, meaning that it cannot be interrupted by another thread. One way to do this is 
+//to use a mutex or a critical section to protect the flag variable or to use an atomic type, 
+//such as std::atomic<bool>, which guarantees that operations on it are indivisible and visible to all threads.
+
 InterruptIn btn(PC_13);
+//declare global flag variable
+bool flag = false;
 
 void btn_interrupt(){
     printf("Button pressed\r\n");
+    //set the flag to true whenr button is pressed
+    flag = true;
 }
 // main() runs in its own thread in the OS
 int main()
 {
     btn.fall(&btn_interrupt);
-    while (true) {
-
+    while (1) {
+        if(flag) {
+            printf("Button presses\r\n");
+            //reste flag after prinitng
+            flag = false;
+        }        
     }
 }
 
